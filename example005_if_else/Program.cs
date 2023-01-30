43943323118cc3a7cfd8898425431202715d1088
Elena Sheveleva <lahjalena@gmail.com>
2023-01-30 20:21:45 +0100

    if else

---
+++ example005_if_else/Program.cs
@@ -0,0 +1,12 @@
+﻿
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+        Console.WriteLine("Ура, это же МАША!");
+}
+else
+{
+        Console.WriteLine("Привет, ");
+        Console.WriteLine(username);
+    }
