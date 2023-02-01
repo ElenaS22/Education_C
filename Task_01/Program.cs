2f36a95d05fb1c33508c983ee3a17a860b4a5ad5
Elena Sheveleva <lahjalena@gmail.com>
2023-02-01 18:56:41 +0100

    задание 1 из семинала 1

---
+++ Task_01/Program.cs
@@ -0,0 +1,10 @@
+﻿//Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+
+int sqr = number * number;
+
+Console.WriteLine($"Квадрат {number} равен {sqr}");
