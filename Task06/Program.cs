0e1dc2e2f47aeb5fe1f133aff55ad3d1b26b3963
Elena Sheveleva <lahjalena@gmail.com>
2023-02-04 09:47:17 +0100

    added task from lesson 2

---
+++ Task06/Program.cs
@@ -0,0 +1,29 @@
+﻿// Поищем максимум из 9 чисел
+
+int Max(int arg1, int arg2, int arg3)
+{
+int result = arg1;
+if (arg2 > result) result = arg2;
+if (arg3 > result) result = arg3;
+return result;
+}
+
+int a1  = 1;
+int b1  = 2;
+int c1  = 6;
+int a2  = 85964;
+int b2  = 4;
+int c2  = 27;
+int a3  = 75;
+int b3  = 4;
+int c3  = 0;
+
+
+// int max1 = Max(a1, b1, c1);
+// int max2 = Max(a2, b2, c2);
+// int max3 = Max(a3, b3, c3);
+// int max = Max(max1, max2, max3);
+
+
+int max = Max(Max(a1, b1, c1), Max(a2, b2, c2), Max(a3, b3, c3));
+Console.Write($" {max}");
