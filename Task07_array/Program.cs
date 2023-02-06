f7212d2a199489fe339c32bfc6ab8240066d362f
Elena Sheveleva <lahjalena@gmail.com>
2023-02-06 14:39:12 +0100

    added about array from lesson2

---
+++ Task07_array/Program.cs
@@ -0,0 +1,26 @@
+﻿// Поищем максимум из 9 чисел
+
+// int Max(int arg1, int arg2, int arg3)
+// {
+// int result = arg1;
+// if (arg2 > result) result = arg2;
+// if (arg3 > result) result = arg3;
+// return result;
+// }
+//             0 1  2 3  4 5  6   7
+int[] array = { 1, 0, 15, 5, 45, 951, 0, 6, 70 };
+int n = array.Length;
+
+int find = 5;
+int index = 0;
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine($"{index}");
+        break; //останавливает выполнение операции
+    }
+    // index = index + 1;
+    index++;
+}
+
